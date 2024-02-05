39901e44268cc0f819b208d12fe75ecd25d6f8a3
Sergey Koksharov <SerjRazrab@gmail.com>
2024-02-05 23:15:33 +0300

    modified domashka_rekurs

--- domashka_rekurs/Program.cs
+++ domashka_rekurs/Program.cs
@@ -1,16 +1,20 @@
 ﻿// Задача 1: Задайте значения M и N. 
 // Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
 // Использовать рекурсию, не использовать циклы.
-
-void rec(int m)
+Console.WriteLine("Введите значение M : ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение N : ");
+int n = Convert.ToInt32(Console.ReadLine());
+void rec(int m,int n)
 {
-if(m!=0)
-{
-    rec(m-1);
-    Console.Write($"{m} ");
+    if (m > n)
+        return;
+    if (m != 0)
+    {
+        Console.Write($"{m} ");
+    }
+    rec(m + 1, n);
 }
-}
-
-rec(500);
+rec(m,n);
 
 
