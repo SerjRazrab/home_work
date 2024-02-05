ca75463a520630dd68dc5d3c558b0e971a1b327a
Sergey Koksharov <SerjRazrab@gmail.com>
2024-02-05 23:09:04 +0300

    create domashka_rekurs_2

---
+++ domashka_rekurs_2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 2.2: Задайте значения M и N. Напишите программу, 
+//которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+Console.WriteLine("Введите значение M : ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение N : ");
+int n = Convert.ToInt32(Console.ReadLine());
+void rec(int m, int n)
+{
+    if (m > n)
+    {
+        return;
+    }
+    if (m % 2 == 0)
+    {
+        Console.Write($"{m} ");
+    }
+    rec(m + 1, n);
+
+}
+
+rec(m, n);
